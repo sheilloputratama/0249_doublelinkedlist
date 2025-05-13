b71863b377b556d4725cfd253e9bfbe3daf243ab
sheilloputratama <sheilloggendut@gmail.com>
2025-05-13 11:46:02 +0700

    pembuatan prosedur delete untuk menghapus data

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -103,3 +103,21 @@ bool search(int rollNo, Node** previous, Node** current)
     return (*current != NULL);
 }
 
+//pembuatan prosedur delete untuk menghapus data
+void deleteNode()
+{
+    Node* previous, * current;
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo; //step 3: get the roll number number to be delete
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+    current = START; // step 1: start from the first node
+    previous = NULL;
+
+    
