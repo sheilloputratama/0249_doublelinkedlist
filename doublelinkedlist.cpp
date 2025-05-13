477c9ce3699d4fc960acbbc5f3a9e9cc05ce28c5
sheilloputratama <sheilloggendut@gmail.com>
2025-05-13 11:46:56 +0700

    membuat node yang akan dihapus

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -120,4 +120,17 @@ void deleteNode()
     current = START; // step 1: start from the first node
     previous = NULL;
 
+    //locate the node to be delete
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+        return;
+    }
+
     
